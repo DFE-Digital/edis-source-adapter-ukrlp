171d35186a0f2de81adb5d778adb332ac98f909d
Simon Bull <simon.bull.84@googlemail.com>
2021-01-04 09:00:37 +0000

    Add App Insights logging

--- src/Dfe.Edis.SourceAdapter.Ukrlp.WebJob/Program.cs
+++ src/Dfe.Edis.SourceAdapter.Ukrlp.WebJob/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Dfe.Edis.SourceAdapter.Ukrlp.Domain.Configuration;
 using Microsoft.Extensions.Configuration;
@@ -36,6 +37,12 @@ namespace Dfe.Edis.SourceAdapter.Ukrlp.WebJob
             {
                 logBuilder.SetMinimumLevel(LogLevel.Debug);
                 logBuilder.AddConsole();
+
+                var appInsightsKey = Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY");
+                if (!string.IsNullOrWhiteSpace(appInsightsKey))
+                {
+                    logBuilder.AddApplicationInsights(appInsightsKey);
+                }
             });
             hostBuilder.ConfigureServices((context, services) =>
             {
